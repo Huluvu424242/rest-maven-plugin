ddd3a6e129dec7c5ff6992caf31f89088bd9c13e
FunThomas424242 <funthomas424242@gmail.com>
2017-01-05 23:19:48 +0100

    junit tests fixed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ src/test/java/com/github/cjnygard/mvn/rest/MojoTestRule.java
@@ -0,0 +1,19 @@
+package com.github.cjnygard.mvn.rest;
+
+import org.apache.maven.plugin.testing.MojoRule;
+
+import java.io.File;
+
+/**
+ * Created by huluvu424242 on 05.01.17.
+ */
+public class MojoTestRule extends MojoRule{
+    @Override
+    protected void before() throws Throwable {
+    }
+
+    @Override
+    protected void after() {
+    }
+
+}
